7a2b19af8a81978cb7bf4a327b5ed323bd29bcbf
Javier-Canaviri <115227969+Javier-Canaviri@users.noreply.github.com>
2024-03-06 13:05:11 +0000

    semana3

--- semana2/restful/server.py
+++ semana2/restful/server.py
@@ -12,12 +12,38 @@ estudiantes = [
 
 
 class RESTRequestHandler(BaseHTTPRequestHandler):
+
+    def response_handler(self,statusCode, data):
+        self.send_response(statusCode)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode("utf-8"))
+
+    def data_reader(self):
+        content_length = int(self.headers["Content-Length"])
+        post_data = self.rfile.read(content_length)
+        post_data = json.loads(post_data.decode("utf-8"))
+        post_data["id"] = len(estudiantes) + 1
+        estudiantes.append(post_data)
+        self.send_response(201)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(estudiantes).encode("utf-8"))
+
+    def find_estudent_by_id(self,id,estudiantes):
+        return next(
+                (estudiante for estudiante in estudiantes if estudiante["id"] == id),
+                None,
+        )
+
+
     def do_GET(self):
         if self.path == "/estudiantes":
-            self.send_response(200)
+            self.response_handler(self,200,estudiantes)
+            """self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(json.dumps(estudiantes).encode("utf-8"))
+            self.wfile.write(json.dumps(estudiantes).encode("utf-8"))"""
              
         elif self.path == "/estudiantes/carreras/":
             estEconomia = next((estudiante["carrera":"Economia"] for estudiante in estudiantes),None,)
@@ -42,15 +68,13 @@ class RESTRequestHandler(BaseHTTPRequestHandler):
                
         elif self.path.startswith("/estudiantes/"):
             id = int(self.path.split("/")[-1])
-            estudiante = next(
-                (estudiante for estudiante in estudiantes if estudiante["id"] == id),
-                None,
-            )
+            estudiante = self.find_estudent_by_id(self,id,estudiantes)
             if estudiante:
-                self.send_response(200)
+                self.response_handler(self,200,estudiante)
+                """self.send_response(200)
                 self.send_header("Content-type", "application/json")
                 self.end_headers()
-                self.wfile.write(json.dumps(estudiante).encode("utf-8"))
+                self.wfile.write(json.dumps(estudiante).encode("utf-8"))"""
         
         else:
             self.send_response(404)
@@ -79,20 +103,13 @@ class RESTRequestHandler(BaseHTTPRequestHandler):
 
     def do_PUT(self):
         if self.path.startswith("/estudiantes"):
-            content_length = int(self.headers["Content-Length"])
-            data = self.rfile.read(content_length)
-            data = json.loads(data.decode("utf-8"))
+            data = self.read_data()            
             id = data["id"]
-            estudiante = next(
-                (estudiante for estudiante in estudiantes if estudiante["id"] == id),
-                None,
-            )
+            estudiante = self.find_estudent_by_id(id)
+            
             if estudiante:
                 estudiante.update(data)
-                self.send_response(200)
-                self.send_header("Content-type", "application/json")
-                self.end_headers()
-                self.wfile.write(json.dumps(estudiante).encode("utf-8"))
+                self.response_handler(200,[estudiantes])
         else:
             self.send_response(404)
             self.send_header("Content-type", "application/json")
