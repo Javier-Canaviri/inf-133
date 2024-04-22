6e24156faae46cecedcf740e652c10b2281472c0
Javier-Canaviri <115227969+Javier-Canaviri@users.noreply.github.com>
2024-04-22 18:02:14 +0000

    clase22/04

---
+++ semana8/mvc/app/models/user_model.py
@@ -0,0 +1,34 @@
+from database import db
+from datetime import date
+# `db.Model` es una clase base para todos los modelos de SQLAlchemy
+# Define la clase `User` que hereda de `db.Model`
+# `User` representa la tabla `users` en la base de datos
+class User(db.Model):
+    __tablename__ = 'users'
+    # Define las columnas de la tabla `users`
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    contraseña = db.Column(db.String(50), nullable=False)
+    fecha_de_nacimiento = db.Column(db.Date()), nullable=False)
+
+    # Inicializa la clase `User`
+    def __init__(self, first_name, last_name,email,contraseña,fecha_de_nacimiento):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.contraseña = contraseña
+        año,mes,dia=map(int,fecha_de_nacimiento, split("/"))
+        self.fecha_de_nacimiento = año,mes,dia
+
+
+    # Guarda un usuario en la base de datos
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    # Obtiene todos los usuarios de la base de datos
+    @staticmethod
+    def get_all():
+        return User.query.all()
