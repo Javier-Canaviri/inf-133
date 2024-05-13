c68826f5f3eaea98ed1a6cb16f3efe5dcb5929c4
Javier-Canaviri <115227969+Javier-Canaviri@users.noreply.github.com>
2024-05-13 18:00:40 +0000

    avance 13/5

---
+++ semana10/decorador/decorador.py
@@ -0,0 +1,19 @@
+from functools import wraps
+def my_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print("Antes de llamar a la funcion")
+        result=func(*args, **kwargs)
+        print("Despues de llamar a la funcion")
+        return result
+    return wrapper
+@my_decorator 
+def greet(name):
+    """Funcion para saludar a alguien"""
+    return(f"Hola, {name}!")
+
+greet("Juan")
+
+print(greet.__name__)
+
+print(greet.__doc__)
